9d88e0549e1890e5b062be123a59dda26b3ebbf7
Sammy <sammysamkough@hotmail.com>
2024-03-08 00:37:10 -0500

    lots of additions: blog, audits, the usage of content collections, rss

---
+++ src/content/config.ts
@@ -0,0 +1,43 @@
+// Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
+const auditsCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    website_url: z.string(),
+    scores: z.object({
+      mobile: z.object({
+        design: z.number(),
+        performance: z.number(),
+        accessibility: z.number(),
+        seo: z.number(),
+      }),
+      desktop: z.object({
+        design: z.number(),
+        performance: z.number(),
+        accessibility: z.number(),
+        seo: z.number(),
+      }),
+    }),
+  }),
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  posts: postsCollection,
+  audits: auditsCollection,
+};
---
+++ src/content/posts/why-websites-matter.md
@@ -0,0 +1,12 @@
+---
+title: "why websites matter"
+pubDate: 2024-03-07
+description: "explains why websites matter and are the key to one's digital identity. any entity, be they a person or business or other"
+author: "sammy samkough"
+image:
+  url: "https://bonny.systems/bowtie.png"
+  alt: "bow tie emoji"
+tags: ["websites"]
+---
+
+n/a
