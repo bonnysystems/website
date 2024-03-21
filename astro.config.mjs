7c1e72befd04a07b40f78b2c9b149bdd81b6cfbe
Sammy <sammysamkough@hotmail.com>
2024-03-21 00:27:57 -0400

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://bonny.systems",
+  integrations: [sitemap()],
   redirects: {
     "/o/[...slug]": "/outreach/[...slug]",
   },
